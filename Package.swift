8ecd5a60d939d2e327b51a909ec6309ed3f8d0d0
xaoxuu <16400144+xaoxuu@users.noreply.github.com>
2024-10-10 18:43:06 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.17.0"),
-        .package(url: "https://github.com/SDWebImage/libwebp-Xcode.git", from: "1.1.0")
+        .package(url: "git@codeup.aliyun.com:6333b695257dab51ddaa62e9/mirrors/SDWebImage.git", branch: "master"),
+        .package(url: "git@codeup.aliyun.com:6333b695257dab51ddaa62e9/mirrors/libwebp-Xcode.git", from: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
